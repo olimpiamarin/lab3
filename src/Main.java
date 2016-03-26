703a1ef5f88a10c093d36a4794be3535a32ef78a
olimpia marin <olimpiamarin84@gmail.com>
2016-03-26 13:29:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Salut lume");
+        System.out.println("Salut lume2");
+        System.out.println("Salut lume3");
+    }
+}
